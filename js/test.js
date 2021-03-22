782da1365e7047eb25edfe6e5542d90e4e3c8585
munmust <17305076869@163.com>
2021-03-22 17:32:39 +0800

    router

--- js/test.js
+++ js/test.js
@@ -490,3 +490,386 @@ function del(str, target) {
     : str.slice(0, index) + str.slice(index + keyword.length);
 }
 console.log(delLast("emamamur, your string", "ur"));
+
+function toCamel(str) {
+  if (str.split("_").length === 1) return;
+  return str.split("_").reduce((a, b) => {
+    console.log(a, b);
+    return a + b.substr(0, 1).toUpperCase() + b.substr(1);
+  });
+}
+
+function remoteSymbol(str) {
+  return str.replace(/[\t\n]/g, "");
+}
+console.log(
+  remoteSymbol(
+    "\t1112 2233\n_aaaaaaa\r\n_bbb bbb\t_3333 333\r_444 4444\n_555555"
+  )
+);
+
+// https://segmentfault.com/a/1190000020736966?utm_source=sf-related
+// https://github.com/KieSun/learn-react-essence/blob/master/%E7%83%AD%E8%BA%AB%E7%AF%87.md
+// https://github.com/jsonz1993/react-source-learn/issues
+// https://slides.com/xuqinggang/title-text#/0/5
+
+const PopStateEvent = "popstate";
+const HashChangeEvent = "hashchange";
+/* 简化了createBrowserHistory，列出了几个核心api及其作用 */
+function createBrowserHistory() {
+  /* 全局history  */
+  const globalHistory = window.history;
+  /* 处理basename */
+  const basename = props.basename
+    ? stripTrailingSlash(addLeadingSlash(props.basename))
+    : "";
+
+  /* 处理路由转换，记录了listens信息 */
+  const transitionManager = createTransitionManager();
+  /* 改变location对象，通知组件更新 */
+  const setState = () => {};
+  /* 处理当path改变后，处理popstate变化的回调函数 */
+  const handlePopState = () => {};
+  /* history.push方法，改变路由，通过全局对象history.pushState改变url, 通知router触发更新，替换组件 */
+  const push = () => {};
+  /* 底层应用事件监听器，监听popstate事件 */
+  const listen = () => {};
+  return {
+    push,
+    listen,
+  };
+}
+let listen = (listener) => {
+  /* 添加监听函数到队列 */
+  const unlisten = transitionManager.appendListener(listener);
+  /* 添加历史记录条目的监听 */
+  checkDomListeners(1);
+  /* 解除监听 */
+  return () => {
+    checkDOMListeners(-1);
+    unlisten();
+  };
+};
+let listenerCount = 0;
+let checkDomListeners = (delta) => {
+  listenerCount += delta;
+
+  if (listenerCount === 1 && delta === 1) {
+    /* 添加绑定，当历史记录条数目改变的时候 */
+    window.addEventListener(PopStateEvent, handlePopState);
+    if (needHashChangeListener) {
+      window.addEventListener(HashChangeEvent, handleHashChange);
+    }
+  } else if (listenerCount === 0) {
+    /* 解除绑定 */
+    window.removeEventListener(PopStateEvent, handleHashChange);
+    if (needHashChangeListener) {
+      window.removeEventListener(HashChangeEvent, handleHashChange);
+    }
+  }
+};
+
+let push = (path, state) => {
+  const action = "PUSH";
+  /* 创建一个新的location */
+  const location = createLocation(path, state, createKey, history.location);
+  /* 确定是否能进行路由转换，还在确认的时候又开始了另一个转变 ,可能会造成异常 */
+  transitionManager.confirmTransitionTo(
+    location,
+    action,
+    getUserConfirmation,
+    (ok) => {
+      if (!ok) return;
+      /* 获取当前路径名 */
+      const href = createHref(location);
+      const { key, state } = location;
+      if (canUseHistory) {
+        globalHistory.pushState({ key, state }, null, href);
+        /* 强制刷新 */
+        if (forceRefresh) {
+          window.location.href = href;
+        } else {
+          const prevIndex = allKeys.indexOf(history.location.key);
+          const nextKeys = allKeys.slice(
+            0,
+            prevIndex === -1 ? 0 : prevIndex + 1
+          );
+
+          nextKeys.push(location.key);
+          allKeys = nextKeys;
+
+          /* 改变 react-router location对象, 创建更新环境 */
+          setState({ action, location });
+        }
+      } else {
+        window.location.href = href;
+      }
+    }
+  );
+};
+
+let handlePopState = (event) => {
+  handlePopState(getDOMLocation(event.state));
+};
+let forceNextPop = false;
+function handlePop(location) {
+  if (forceNextPop) {
+    forceNextPop = false;
+    setState();
+  } else {
+    const action = "POP";
+    transitionManager.confirmTransitionTo(
+      location,
+      action,
+      getUserConfirmation,
+      (ok) => {
+        if (ok) {
+          setState({ action, location });
+        } else {
+          revertPop(location);
+        }
+      }
+    );
+  }
+}
+
+function getHistoryState() {
+  try {
+    return window.history.state || {};
+  } catch (e) {
+    return {};
+  }
+}
+/**
+ * 处理state参数和window.location
+ * @param historyState
+ * @returns {{hash, key, pathname, search, state}}
+ */
+function getDOMLocation(historyState) {
+  const { key, state } = historyState || {};
+  const { pathname, search, hash } = window.location;
+
+  let path = pathname + search + hash;
+
+  // 保证path是不包含basename的
+  if (basename) path = stripBasename(path, basename);
+
+  // 创建history.location对象
+  return createLocation(path, state, key);
+}
+function listen(listener) {
+  const unlisten = transitionManager.appendListener(listener);
+  checkDOMListeners(1);
+
+  return () => {
+    checkDOMListeners(-1);
+    unlisten();
+  };
+}
+
+let listenerCount = 0;
+function checkDOMListeners(delta) {
+  listenerCount += delta;
+
+  if (listenerCount === 1 && delta === 1) {
+    window.addEventListener(HashChangeEvent, handleHashChange);
+  } else if (listenerCount === 0) {
+    window.removeEventListener(HashChangeEvent, handleHashChange);
+  }
+}
+
+function setState(nextState) {
+  Object.assign(history, nextState);
+  history.length = globalHistory.length;
+  transitionManager.notifyListeners(history.location, history.action);
+}
+
+function push(path, state) {
+  warning(state === undefined, "Hash history cannot push state; it is ignored");
+
+  const action = "PUSH";
+  const location = createLocation(path, undefined, undefined, history.location);
+
+  transitionManager.confirmTransitionTo(
+    location,
+    action,
+    getUserConfirmation,
+    (ok) => {
+      if (!ok) return;
+
+      const path = createPath(location);
+      const encodedPath = encodePath(basename + path);
+      const hashChanged = getHashPath() !== encodedPath;
+
+      if (hashChanged) {
+        ignorePath = path;
+        pushHashPath(encodedPath);
+        const prevIndex = allPaths.lastIndexOf(createPath(history.location));
+        const nextPaths = allPaths.slice(
+          0,
+          prevIndex === -1 ? 0 : prevIndex + 1
+        );
+        nextPaths.push(path);
+        allPaths = nextPaths;
+        setState({ action, location });
+      } else {
+        setState();
+      }
+    }
+  );
+}
+function pushHashPath(path) {
+  window.location.hash = path;
+}
+function replace(path, state) {
+  const action = "REPLACE";
+  const location = createLocation(
+    path,
+    undefined,
+    undefined,
+    history.location
+  );
+  transitionManager.confirmTransitionTo(
+    location,
+    action,
+    getUserConfirmation,
+    ok => {
+      if (!ok) return;
+      const path = createPath(location);
+      const encodedPath = encodePath(basename + path);
+      const hashChanged = getHashPath() !== encodedPath;
+      if (hashChanged) {
+        ignorePath = path;
+        replaceHashPath(encodedPath);
+      }
+      const prevIndex = allPaths.indexOf(createPath(history.location));
+      if (prevIndex !== -1) allPaths[prevIndex] = path;
+      setState({ action, location });
+    }
+  );
+}
+function replaceHashPath(path) {
+  const hashIndex = window.location.href.indexOf("#");
+  window.location.replace(
+    window.location.href.slice(0, hashIndex >= 0 ? hashIndex : 0) + "#" + path
+  );
+}
+class Route extends React.Component {
+  render() {
+    return (
+      <RouterContext.Consumer>
+        {context => {
+          /* router / route 会给予警告警告 */
+          invariant(context, "You should not use <Route> outside a <Router>");
+          // computedMatch 为 经过 swich处理后的 path
+          const location = this.props.location || context.location;
+          const match = this.props.computedMatch 
+            ? this.props.computedMatch // <Switch> already computed the match for us
+            : this.props.path
+            ? matchPath(location.pathname, this.props)
+            : context.match;
+          const props = { ...context, location, match };
+          let { children, component, render } = this.props;
+
+          if (Array.isArray(children) && children.length === 0) {
+            children = null;
+          }
+
+          return (
+            <RouterContext.Provider value={props}>
+              {props.match
+                ? children
+                  ? typeof children === "function"
+                    ? __DEV__
+                      ? evalChildrenDev(children, props, this.props.path)
+                      : children(props)
+                    : children
+                  : component
+                  ? React.createElement(component, props)
+                  : render
+                  ? render(props)
+                  : null
+                : typeof children === "function"
+                ? __DEV__
+                  ? evalChildrenDev(children, props, this.props.path)
+                  : children(props)
+                : null}
+            </RouterContext.Provider>
+          );
+        }}
+      </RouterContext.Consumer>
+    );
+  }
+}
+
+class Switch extends React.Component {
+  render() {
+    return (
+      <RouterContext.Consumer>
+        {/* 含有 history location 对象的 context */}
+        {context => {
+          invariant(context, 'You should not use <Switch> outside a <Router>');
+          const location = this.props.location || context.location;
+          let element, match;
+          //我们使用React.Children.forEach而不是React.Children.toArray（）.find（）
+          //这里是因为toArray向所有子元素添加了键，我们不希望
+          //为呈现相同的两个<Route>s触发卸载/重新装载
+          //组件位于不同的URL。
+          //这里只需然第一个 含有 match === null 的组件
+          React.Children.forEach(this.props.children, child => {
+            if (match == null && React.isValidElement(child)) {
+              element = child;
+              // 子组件 也就是 获取 Route中的 path 或者 rediect 的 from
+              const path = child.props.path || child.props.from;
+              match = path
+                ? matchPath(location.pathname, { ...child.props, path })
+                : context.match;
+            }
+          });
+          return match
+            ? React.cloneElement(element, { location, computedMatch: match })
+            : null;
+        }}
+      </RouterContext.Consumer>
+    );
+  }
+}
+/* 匹配路由 */
+function matchPath(pathname, options = {}) {
+  if (typeof options === "string" || Array.isArray(options)) {
+    options = { path: options };
+  }
+
+  const { path, exact = false, strict = false, sensitive = false } = options;
+
+  const paths = [].concat(path);
+
+  return paths.reduce((matched, path) => {
+    if (!path && path !== "") return null;
+    if (matched) return matched;
+
+    const { regexp, keys } = compilePath(path, {
+      end: exact,
+      strict,
+      sensitive
+    });
+    const match = regexp.exec(pathname);
+    /* 匹配不成功，返回null */
+    if (!match) return null;
+
+    const [url, ...values] = match;
+    const isExact = pathname === url;
+
+    if (exact && !isExact) return null;
+
+    return {
+      path, // the path used to match
+      url: path === "/" && url === "" ? "/" : url, // the matched portion of the URL
+      isExact, // whether or not we matched exactly
+      params: keys.reduce((memo, key, index) => {
+        memo[key.name] = values[index];
+        return memo;
+      }, {})
+    };
+  }, null);
+}
